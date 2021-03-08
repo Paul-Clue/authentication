52d74665647df8eec62600d167feba53d8942511
unknown <paulclue20@gmail.com>
2021-03-08 18:25:45 -0500

    remove un-needed file

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,11 +1,11 @@
 module PostsHelper
   def post_list(post)
-      data = ""
-      if user_signed_in? 
-        post.each do |post|
-          data += "<p> <strong>#{post.title}</strong> <br>
+    data = ''
+    if user_signed_in?
+      post.each do |post|
+        data += "<p> <strong>#{post.title}</strong> <br>
           #{post.user.email}</br>#{post.post}</p>"
-        end
+      end
     else
       post.each do |post|
         data += "<p> <strong>#{post.title}</strong><br>#{post.post} </p>"
