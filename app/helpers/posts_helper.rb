3b57634a57bee37fe61c7d5271ab99ca059b976c
unknown <paulclue20@gmail.com>
2021-03-08 18:27:57 -0500

    correct linter errors

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -2,13 +2,13 @@ module PostsHelper
   def post_list(post)
     data = ''
     if user_signed_in?
-      post.each do |post|
-        data += "<p> <strong>#{post.title}</strong> <br>
-          #{post.user.email}</br>#{post.post}</p>"
+      post.each do |posts|
+        data += "<p> <strong>#{posts.title}</strong> <br>
+          #{posts.user.email}</br>#{posts.post}</p>"
       end
     else
-      post.each do |post|
-        data += "<p> <strong>#{post.title}</strong><br>#{post.post} </p>"
+      post.each do |posts|
+        data += "<p> <strong>#{posts.title}</strong><br>#{posts.post} </p>"
       end
     end
     data.html_safe
