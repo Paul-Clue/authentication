37ff8567142b358d4d7834f8565ca7f413580bdd
unknown <paulclue20@gmail.com>
2021-03-08 16:47:50 -0500

    remove logic from view

--- app/helpers/post_list.rb
+++ app/helpers/post_list.rb
@@ -1,107 +1,134 @@
 module PostLister
+  # def post_list(post)
+  #    if user_signed_in?
+  #     <table>
+  #       <thead>
+  #         <tr>
+  #           <th>Post</th>
+  #           <th colspan="3"></th>
+  #         </tr>
+  #       </thead>
+
+  #       <tbody>
+  #        @posts.each do |post|
+  #         <tr>
+  #           <td><strong><> post.title</strong></td>
+  #         </tr>
+  #         <tr>
+  #         <td>post.user.email</td>
+  #         </tr>
+  #         <tr>
+  #         <td>post.post </td>
+  #         </tr>
+  #         <tr>
+  #         <td></td>
+  #         </tr>
+  #         <tr>
+  #         <td></td>
+  #         </tr>
+  #         <tr>
+  #         <td></td>
+  #         </tr>
+  #           end
+  #       </tbody>
+  #     </table>
+
+  #      else 
+  #     <table>
+  #       <thead>
+  #         <tr>
+  #           <th>Post</th>
+  #           <th colspan="3"></th>
+  #         </tr>
+  #       </thead>
+
+  #         <tbody>
+  #            @posts.each do |post| 
+  #             <tr>
+  #               <td><strong>post.title</strong></td>
+  #               </tr>
+  #             <tr>
+  #               <td>post.post</td>
+  #             </tr>
+  #             <tr>
+  #                 <td></td>
+  #               </tr>
+  #               <tr>
+  #                 <td></td>
+  #               </tr>
+  #               <tr>
+  #                 <td></td>
+  #               </tr>
+  #               <tr>
+  #                 <td></td>
+  #               </tr>  
+  #               <tr>
+  #                 <td></td>
+  #               </tr>
+  #               <tr>
+  #                 <td></td>
+  #               </tr>
+  #               <tr>
+  #                 <td></td>
+  #               </tr>
+  #               <tr>
+  #                 <td></td>
+  #               </tr>
+  #               <tr>
+  #                 <td></td>
+  #               </tr>
+  #               <tr>
+  #                 <td></td>
+  #               </tr>
+  #               <tr>
+  #                 <td></td>
+  #               </tr>
+  #               <tr>
+  #                 <td></td>
+  #               </tr>
+  #             end
+  #         </tbody>
+  #     </table>
+  #     end
+
+  #   <br>
+
+
+  #   if user_signed_in?
+  #     <%= link_to 'New Post', new_post_path %><br />
+  #     <%= link_to "Logout", destroy_user_session_path, method: :delete, class:"button is-info" %>
+  #     else
+  #      link_to 'Sign In', new_user_session_path, class: "button is-info" <br />
+  #     <%= link_to 'Sign Up', new_user_registration_path, class: "button is-info" %>
+  #   end
+  # end
+
+
+
   def post_list(post)
-     if user_signed_in?
-      <table>
-        <thead>
-          <tr>
-            <th>Post</th>
-            <th colspan="3"></th>
-          </tr>
-        </thead>
-
-        <tbody>
-         @posts.each do |post|
-          <tr>
-            <td><strong><> post.title</strong></td>
-          </tr>
-          <tr>
-          <td>post.user.email</td>
-          </tr>
-          <tr>
-          <td>post.post </td>
-          </tr>
-          <tr>
-          <td></td>
-          </tr>
-          <tr>
-          <td></td>
-          </tr>
-          <tr>
-          <td></td>
-          </tr>
-            end
-        </tbody>
-      </table>
-
-       else 
-      <table>
-        <thead>
-          <tr>
-            <th>Post</th>
-            <th colspan="3"></th>
-          </tr>
-        </thead>
-
-          <tbody>
-             @posts.each do |post| 
-              <tr>
-                <td><strong>post.title</strong></td>
-                </tr>
-              <tr>
-                <td>post.post</td>
-              </tr>
-              <tr>
-                  <td></td>
-                </tr>
-                <tr>
-                  <td></td>
-                </tr>
-                <tr>
-                  <td></td>
-                </tr>
-                <tr>
-                  <td></td>
-                </tr>  
-                <tr>
-                  <td></td>
-                </tr>
-                <tr>
-                  <td></td>
-                </tr>
-                <tr>
-                  <td></td>
-                </tr>
-                <tr>
-                  <td></td>
-                </tr>
-                <tr>
-                  <td></td>
-                </tr>
-                <tr>
-                  <td></td>
-                </tr>
-                <tr>
-                  <td></td>
-                </tr>
-                <tr>
-                  <td></td>
-                </tr>
-              end
-          </tbody>
-      </table>
-      end
-
-    <br>
-
-
     if user_signed_in?
-      <%= link_to 'New Post', new_post_path %><br />
-      <%= link_to "Logout", destroy_user_session_path, method: :delete, class:"button is-info" %>
-      else
-       link_to 'Sign In', new_user_session_path, class: "button is-info" <br />
-      <%= link_to 'Sign Up', new_user_registration_path, class: "button is-info" %>
+      (0..5) do |post|
+        post.title
+        post.user.email
+        post.post
+        
+    else
+      (0..5) do |post| 
+        <strong>post.title</strong>
+        post.post
     end
   end
+   <br>
+
+
+#    if user_signed_in?
+#      <%= link_to 'New Post', new_post_path %><br />
+#      <%= link_to "Logout", destroy_user_session_path, method: :delete, class:"button is-info" %>
+#      else
+#       link_to 'Sign In', new_user_session_path, class: "button is-info" <br />
+#      <%= link_to 'Sign Up', new_user_registration_path, class: "button is-info" %>
+#    end
+#  end
 end
  
  
--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,18 @@
 module PostsHelper
+  def post_list(post)
+    if user_signed_in?
+      post.each do
+        # render inline: '<p> post.title </p>'
+        # render inline: '<p> post.user.email </p>'
+        # render inline: '<p> post.post </p>'
+        post.map { |item| tag.p(item.title) }.join.html_safe
+      end
+    else
+      post.each do
+        # render inline: '<p> post.title </p>'
+        # render inline: '<p> post.post </p>'
+        post.map { |item| tag.p(item.user.email) }.join.html_safe
+      end
+    end
+  end
 end
