f1ffe699f065ccec06e30233af31954e95ff0fd8
Carlos Ospina <carlossospinag@gmail.com>
2021-03-09 12:25:29 -0500

    update navigation links in helper files

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,2 @@
 module ApplicationHelper
-    def my_link_to(text, href)
-        "<a href='#{href}'>#{text}</a>".html_safe
-      end
 end
--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,16 +1,22 @@
 module PostsHelper
   def post_list(post)
     data = ''
+    a = render inline: "<%= link_to 'Sign_out', destroy_user_session_path, method: :delete %> <br>"
+    b = render inline: "<%= link_to 'New post', new_post_path %> <br>"
+    c = render inline: "<%= link_to 'Log In', new_user_session_path %> <br>"
+    d = render inline: "<%= link_to 'Sign Up', new_user_registration_path %> <br>"
+
     if user_signed_in?
       post.each do |posts|
         data += "<p> <strong>#{posts.title}</strong> <br>
           #{posts.user.email}</br>#{posts.post}</p>"
       end
+      return data.html_safe + b + a
     else
       post.each do |posts|
         data += "<p> <strong>#{posts.title}</strong><br>#{posts.post} </p>"
       end
     end
-    data.html_safe
+    data.html_safe + c + d
   end
 end
